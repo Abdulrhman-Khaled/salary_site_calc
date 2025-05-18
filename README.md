54674660f1381ad204b2ee6e113059a98c8e9809
frappe <frappe@srv715558>
2025-05-18 09:09:28 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Salary Site Calc
+
+Calc salary per day based on shift type
+
+#### License
+
+MIT
---
+++ salary_site_calc/config/docs.py
@@ -0,0 +1,10 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/salary_site_calc"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Salary Site Calc"
