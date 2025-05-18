061f281e2614fdc475898d15d5c1758ba8cb5d13
Abdulrhman Khaled <bodyono3@gmail.com>
2025-05-18 16:01:04 +0300

    edit

--- salary_site_calc/overrides/salary_slip/salary_slip.js
+++ salary_site_calc/overrides/salary_slip/salary_slip.js
@@ -31,7 +31,11 @@ frappe.ui.form.on('Salary Slip', {
             filters: { 'employee': employee },
             limit_page_length: 1
         }).then((salary_doc) => {
-            console.log(salary_doc[0].custom_site_percentage);
+            frappe.get_doc('Salary Structure Assignment', salary_doc[0])
+                .then(doc => {
+                    const fieldValue = doc.custom_site_percentage;
+                    console.log(fieldValue);
+                });
         });
     }
 });
