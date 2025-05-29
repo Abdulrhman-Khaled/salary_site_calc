d4f5bcf2aaa9c1a82905242a82aed2d2c931597a
Abdulrhman Khaled <bodyono3@gmail.com>
2025-05-29 12:06:55 +0300

    edit hooks

--- salary_site_calc/hooks.py
+++ salary_site_calc/hooks.py
@@ -160,7 +160,8 @@ doctype_js = {
 # ------------------------------
 #
 override_whitelisted_methods = {
-	"hrms.hr.doctype.employee_attendance_tool.employee_attendance_tool.mark_employee_attendance": "salary_site_calc.overrides.employee_attendance_tool.custom_mark_employee_attendance"
+	"hrms.hr.doctype.employee_attendance_tool.employee_attendance_tool.mark_employee_attendance": "salary_site_calc.overrides.employee_attendance_tool.employee_attendance_tool."
+    "custom_mark_employee_attendance"
 }
 #
 # each overriding function accepts a `data` argument;
